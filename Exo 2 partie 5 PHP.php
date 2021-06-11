da850f10e02a385f6a0b152bb3747470632fca2b
rattanaksar <81554110+rattanaksar@users.noreply.github.com>
2021-06-11 09:35:56 +0200

    Fichier PHP

---
+++ Exo 2 partie 5 PHP.php
@@ -0,0 +1,18 @@
+<?php
+    $string = 'Bonjour la manu nous sommes le vendredi 11 juin 2021'
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exo 2 : Les fonctions</title>
+</head>
+<body>
+    <h1>Exo 2 : Les fonctions</h1>
+    <p>Faire une fonction qui prend en paramètre une chaine de caractères et qui retourne cette même chaine.<p>
+        <p><?=$string?><p>
+    </p>
+</body>
+</html>
